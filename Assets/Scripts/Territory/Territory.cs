72154f136897cedb59872bb70fa934c2b3641570
WilliamAB <liams_1998@hotmail.com>
2022-04-27 04:47:04 +0300

    Create simple territory script

---
+++ Assets/Scripts/Territory/Territory.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Territory
+{
+    public class Territory : MonoBehaviour
+    {
+        // private 
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+
+        }
+
+
+        private void OnTriggerExit2D(Collider2D other)
+        {
+
+        }
+    }
+}

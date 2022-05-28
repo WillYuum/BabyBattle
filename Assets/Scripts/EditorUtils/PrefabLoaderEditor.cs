cbf9d94e1ff7b082518f5204a0fb8c1ad34cf013
WilliamAB <liams_1998@hotmail.com>
2022-05-29 01:04:56 +0300

    Make PrefabLoader return gameobject instead of T type of monobehavior

--- Assets/Scripts/EditorUtils/PrefabLoaderEditor.cs
+++ Assets/Scripts/EditorUtils/PrefabLoaderEditor.cs
@@ -1,56 +1,58 @@
+// Got this code from:
+// https://gist.github.com/HilariousCow/7f301b04c28fdf61e71f
+
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 using System.IO;
 
-public static class PrefabLoader
+namespace EditorExtensions.Prefabs
 {
-    //So, there's no "load all assets in directory" function in unity. 
-    //I guess this is to avoid people using Prefabs as "data blobs". 
-    //They'd rather you use ScriptableObjects... which is fine in some cases, 
-    //but sometimes the thing you're blobbing is just a bunch of child transforms anyway,
-    //so it's huge buckets of sweat to reproduce the scene tools unity already has. 
-
-    //The "AssetsDatabase.LoadAllAssetsAtPath" refers to /compound/ raw assets,
-    //like maya files. But it doesn't care about humble prefabs, 
-    //which are more like compounds of assets, rather than raw assets.
-
-    //This function collates all the the behaviours you want in the directory you point it at. The path is relative to your Assets.
-    //i.e. "Assets/MyDirectory/"
-    //It returns the Prefab References, remember! Not instantiated scene objects! 
-    //So it's only used for *editor-side* tools. Not run time.
-    //Quite useful in conjunction with "PrefabUtility".
-    public static List<T> LoadAllPrefabsOfType<T>(string path) where T : MonoBehaviour
+    public static class PrefabLoader
     {
-        if (path != "")
+        //So, there's no "load all assets in directory" function in unity. 
+        //I guess this is to avoid people using Prefabs as "data blobs". 
+        //They'd rather you use ScriptableObjects... which is fine in some cases, 
+        //but sometimes the thing you're blobbing is just a bunch of child transforms anyway,
+        //so it's huge buckets of sweat to reproduce the scene tools unity already has. 
+
+        //The "AssetsDatabase.LoadAllAssetsAtPath" refers to /compound/ raw assets,
+        //like maya files. But it doesn't care about humble prefabs, 
+        //which are more like compounds of assets, rather than raw assets.
+
+        //This function collates all the the behaviours you want in the directory you point it at. The path is relative to your Assets.
+        //i.e. "Assets/MyDirectory/"
+        //It returns the Prefab References, remember! Not instantiated scene objects! 
+        //So it's only used for *editor-side* tools. Not run time.
+        //Quite useful in conjunction with "PrefabUtility".
+        public static List<GameObject> LoadAllPrefabsOfType(string path)
         {
-            if (path.EndsWith("/"))
+            if (path != "")
             {
-                path = path.TrimEnd('/');
-            }
-        }
-
-        DirectoryInfo dirInfo = new DirectoryInfo(path);
-        FileInfo[] fileInf = dirInfo.GetFiles("*.prefab");
-
-        //loop through directory loading the game object and checking if it has the component you want
-        List<T> prefabComponents = new List<T>();
-        foreach (FileInfo fileInfo in fileInf)
-        {
-            string fullPath = fileInfo.FullName.Replace(@"\", "/");
-            string assetPath = "Assets" + fullPath.Replace(Application.dataPath, "");
-            GameObject prefab = AssetDatabase.LoadAssetAtPath(assetPath, typeof(GameObject)) as GameObject;
-
-            if (prefab != null)
-            {
-                T hasT = prefab.GetComponent<T>();
-                if (hasT != null)
+                if (path.EndsWith("/"))
                 {
-                    prefabComponents.Add(hasT);
+                    path = path.TrimEnd('/');
                 }
             }
-        }
-        return prefabComponents;
-    }
 
+            DirectoryInfo dirInfo = new DirectoryInfo(path);
+            FileInfo[] fileInf = dirInfo.GetFiles("*.prefab");
+
+            //loop through directory loading the game object and checking if it has the component you want
+            List<GameObject> prefabs = new List<GameObject>();
+            foreach (FileInfo fileInfo in fileInf)
+            {
+                string fullPath = fileInfo.FullName.Replace(@"\", "/");
+                string assetPath = "Assets" + fullPath.Replace(Application.dataPath, "");
+                GameObject prefab = AssetDatabase.LoadAssetAtPath(assetPath, typeof(GameObject)) as GameObject;
+
+                if (prefab != null)
+                {
+                    prefabs.Add(prefab);
+                }
+            }
+            return prefabs;
+        }
+
+    }
 }

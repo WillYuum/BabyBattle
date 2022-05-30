988e44a43ec9ef42527d559b5be4dc56bbe5649b
WilliamAB <liams_1998@hotmail.com>
2022-05-31 00:03:07 +0300

    Create WorldManager script + split logic for camera position bound & world bounds

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -11,53 +11,52 @@ namespace CameraControllerCore
         private CameraStateCore _currentCameraState;
 
 
-        [Header("References")]
-        [SerializeField] private Transform _cameraMinPosition;
-        [SerializeField] private Transform _cameraMaxPosition;
+        private Vector3 _cameraMinPosition;
+        private Vector3 _cameraMaxPosition;
 
 
         void Awake()
         {
-
-            Vector2 cameraCurrentPosition = Camera.main.transform.position;
-            float cameraHalfWidthLength = Camera.main.ViewportToWorldPoint(new Vector2(1.0f, 0f)).x;
-            //Need to do the calculation when camera is set in origin position
-            //so removing unwanted extra length
-            cameraHalfWidthLength -= cameraCurrentPosition.x;
-
-            void addOffsetToMinMaxPosition(Transform bounds, float offset)
-            {
-                bounds.position += new Vector3(offset, 0f, 0f);
-            }
-
-            addOffsetToMinMaxPosition(_cameraMinPosition, cameraHalfWidthLength);
-            addOffsetToMinMaxPosition(_cameraMaxPosition, -cameraHalfWidthLength);
-
-
-
+            SetCameraPositionBound();
 
             _currentCameraState = _followPlayerCameraState;
             _followPlayerCameraState.Init(this);
         }
-
-        private void SetCameraOffSetToBoundsOnCamera(Transform bounds, Vector3 cameraOffset)
-        {
-            Vector3 positions = bounds.position;
-            positions.x += cameraOffset.x;
-            bounds.position = positions;
-        }
-
 
         void Update()
         {
             _currentCameraState.Execute();
         }
 
+
+        private void SetCameraPositionBound()
+        {
+            Vector2 cameraCurrentPosition = Camera.main.transform.position;
+            float cameraHalfWidthLength = Camera.main.ViewportToWorldPoint(new Vector2(1.0f, 0f)).x;
+            //Need to do the calculation when camera is set in origin position
+            //so removing unwanted extra length
+            cameraHalfWidthLength -= cameraCurrentPosition.x;
+
+            var worldBorders = WorldManager.instance.WorldBorders;
+            _cameraMinPosition = worldBorders.MaxLeft.position;
+            _cameraMaxPosition = worldBorders.MaxRight.position;
+
+            void addOffsetToMinMaxPosition(ref Vector3 bounds, float offset)
+            {
+                bounds += new Vector3(offset, 0f, 0f);
+            }
+
+            addOffsetToMinMaxPosition(ref _cameraMinPosition, cameraHalfWidthLength);
+            addOffsetToMinMaxPosition(ref _cameraMaxPosition, -cameraHalfWidthLength);
+        }
+
+
+
         public void CameraFollowPlayer()
         {
             Vector3 newCameraPos = MainCharacter.instance.GetPos;
 
-            newCameraPos.x = Mathf.Clamp(newCameraPos.x, _cameraMinPosition.position.x, _cameraMaxPosition.position.x);
+            newCameraPos.x = Mathf.Clamp(newCameraPos.x, _cameraMinPosition.x, _cameraMaxPosition.x);
             newCameraPos.y = transform.position.y;
             newCameraPos.z = transform.position.z;
 
